d7701db89efe1cc349bdffa4968c487df677493f
Henry Steffensen <henry@steffensenfamily.com>
2021-12-03 13:37:46 +0100

    day 1

--- src/Utils.kt
+++ src/Utils.kt
@@ -6,6 +6,9 @@ import java.security.MessageDigest
  * Reads lines from the given input txt file.
  */
 fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun List<String>.asInts(): List<Int> {
+    return map { it.toInt() }
+}
 
 /**
  * Converts string to md5 hash.
---
+++ src/day01/Day01.kt
@@ -0,0 +1,34 @@
+package day01
+
+import asInts
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int =
+        input.asInts().zipWithNext()
+            .map { it.second - it.first }
+            .count { it > 0 }
+
+    fun part2(input: List<String>): Int =
+        input.asInts()
+            .zip(input.slice(1 until input.size).asInts().zipWithNext()) { a, b -> Triple(a, b.first, b.second) }
+            .also { println(it) }
+            .map { it.toList().sum() }.zipWithNext()
+            .map { it.second - it.first }
+            .count { it > 0 }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("day01/Day01_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
+
+    val input = readInput("day01/Day01")
+    println(part1(input))
+
+    val wrongPart2Answers = listOf(
+        1659,
+    )
+    println(part2(input)
+        .also { check(!wrongPart2Answers.contains(it)) { "Wrong answer for part 2: $it" } }
+    )
+}
